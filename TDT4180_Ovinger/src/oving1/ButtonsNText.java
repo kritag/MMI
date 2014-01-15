0042f769a9b6a4488476d1c2db3b6662c31e5af3
vonVaffel <tagesen.kristian@gmail.com>
2014-01-15 17:29:47 +0100

    1st.

---
+++ TDT4180_Ovinger/src/oving1/ButtonsNText.java
@@ -0,0 +1,30 @@
+package oving1;
+
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JToggleButton;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ButtonsNText extends JPanel{
+	
+	public JToggleButton upperCaseButton;
+	public JToggleButton lowerCaseButton;
+	public JCheckBox continuousButton;
+	public JTextField textLine;
+	
+	public ButtonsNText () {
+		
+	}
+	
+	public static void Main (String[] args){
+		
+	}
+}
